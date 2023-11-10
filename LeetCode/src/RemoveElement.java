41c003666df388f6d15c8c3a38e468176b373202
petrlarionov <petrlarionov@192.168.1.99>
2023-11-10 17:02:59 +0200

    Initial commit

---
+++ LeetCode/src/RemoveElement.java
@@ -0,0 +1,13 @@
+
+
+/*		https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150		*/
+
+public class RemoveElement {
+
+	 public int removeElement(int[] nums, int val) {
+	        
+		 return 0;
+	 
+	 }
+	
+}
